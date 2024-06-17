67ada1cb8adc767a813b4775ffd0e892e0748d62
Rupesh Kumar Dash <rupeshkumardash@Rupeshs-MacBook-Pro.local>
2024-06-17 23:06:30 +0530

    dependencies added

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,3 +1,3 @@
 "use strict";
-const a = 6;
 console.log("hello world");
+console.log("This is my first ts file");
---
+++ Backend/src/index.ts
@@ -0,0 +1,2 @@
+console.log("hello world");
+console.log("This is my first ts file");
