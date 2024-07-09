0e858b5897cbc637d67a40f79e8dc67904d127c6
Rupesh Kumar Dash <rupeshkumardash@Rupeshs-MacBook-Pro.local>
2024-07-09 12:05:37 +0530

    merge commit

--- frontend/src/components/taskComponent/TaskWrapper.tsx
+++ frontend/src/components/taskComponent/TaskWrapper.tsx
@@ -1,16 +1,27 @@
 import Header from "../designConstants/Header";
 import Navigation from "../designConstants/Navigation";
+import Backlog from "./TaskType/Backlog";
 
 const TaskWrapper = () => {
   return (
-    <div className="page-wrapper">
+      <div className="page-wrapper">
       <Navigation />
       <div className="inner-wrapper">
         {" "}
         <Header />
-        
+        <section className="team-section">
+          <header className="section-header my-7 px-5">Tasks</header>
+          <div className="task-wrapper">
+            <Backlog/>
+            <Backlog/>
+            <Backlog/>
+            <Backlog/>
+          </div>
+        </section>
       </div>
     </div>
+
+   
   );
 };
 
